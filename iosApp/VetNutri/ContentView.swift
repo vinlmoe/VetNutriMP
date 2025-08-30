cbc3825a68a2b771c17d39429b2be47e3cd62ad8
vinlmoe <sebastien.lefebvre@vetbrain.fr>
2025-08-30 14:22:53 +0200

    bien fonction

---
+++ iosApp/VetNutri/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  VetNutri
+//
+//  Created by Sebastien Lefebvre on 29/08/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
