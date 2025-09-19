27562b3e1b5629469fa7ab734ab1c5a28b65ce83
vinlmoe <sebastien.lefebvre@vetbrain.fr>
2025-09-19 08:29:59 +0200

    bien corrr à

--- composeApp/src/commonMain/kotlin/fr/vetbrain/vetnutri_mp/Enumer/NutrientAnalysis.kt
+++ composeApp/src/commonMain/kotlin/fr/vetbrain/vetnutri_mp/Enumer/NutrientAnalysis.kt
@@ -9,13 +9,13 @@ enum class NutrientAnalysis(
 
 ) : Nutrient {
     NaK("Rapport K/NA", 0, "", "KNA"),
-    PCa("Rapport phosphocalcique", 1, "", "CAP"),
-    o6o3("Rapport omega 6/omega3", 2, "", "O6O3"),
+    PCa("Rapport Ca/P", 1, "", "CAP"),
+    o6o3("Rapport O6/O3", 2, "", "O6O3"),
     ZnCu("Rapport Zn/Cu", 3, "", "ZNCU"),
     nonOsPhos("Phosphore non osseux", 7, "%", "nonOsPhos"),
     nonOsProt("Proteine non osseuse", 8, "%", "nonOsProt"),
     nonOsPP("Ratio Prot/phos non osseux", 9, "", "nonOsPP"),
-    PhosphProt("Rapport Protéines/Phosphore", 4, "", "PROTP"),
+    PhosphProt("Rapport Prot/Phos", 4, "", "PROTP"),
     MethCys("Méthionine+cystéine", 5, "g", "METHCYS"),
     PhenTyr("Phénylalanine+tyrosine", 6, "g", "PHENTYR");
 
