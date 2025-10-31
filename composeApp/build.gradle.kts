f8a4ae5a88e1bc4d79929b463e08837cc81d708b
vinlmoe <sebastien.lefebvre@vetbrain.fr>
2025-10-31 11:34:03 +0100

    cal 1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -60,6 +60,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation("org.jetbrains.compose.ui:ui-util:1.7.0")
             implementation(libs.kotlinx.coroutines.core)
